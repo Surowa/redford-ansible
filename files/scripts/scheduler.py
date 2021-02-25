53b8b62a01837686369b8bbbd0446fa073bac84e
Surowa <robertwagensveld2@live.nl>
2021-02-25 18:05:36 +0100

    Scheduler with new lamps

--- files/scripts/scheduler.py
+++ files/scripts/scheduler.py
@@ -5,7 +5,7 @@ import time
 import os
 import datetime
 from suntime import Sun, SunTimeException
-timedelta = 1 #Number of hours beyond GMT, in summertime extra hour because of daylight saving
+timedelta = 1 #Number of hours beyond GMT, in summertime extra hour because of daylight saving, then make it 2 temporarily
 latitude = 50.89
 longitude = 5.75
 sun = Sun(latitude, longitude)
@@ -41,10 +41,34 @@ def WaterkokerAan():
     return
 
 def AqualichtAan():
-    os.system('sudo pilight-send -p kaku_switch -i 25544186 -u 0 -t')
+    os.system('sudo pilight-send -p kaku_switch -i 38045566 -u 2 -t')
     return
 
 def AquaLichtUit():
+    os.system('sudo pilight-send -p kaku_switch -i 38045566 -u 2 -f')
+    return
+
+def BuitenLichtAan():
+    os.system('sudo pilight-send -p kaku_switch -i 38045566 -u 0 -t')
+    return
+
+def BuitenLichtUit():
+    os.system('sudo pilight-send -p kaku_switch -i 38045566 -u 0 -f')
+    return
+
+def TvMeubelLampAan():
+    os.system('sudo pilight-send -p kaku_switch -i 38045566 -u 1 -t')
+    return
+
+def TvMeubelLampUit():
+    os.system('sudo pilight-send -p kaku_switch -i 38045566 -u 1 -f')
+    return
+
+def LampNaastBankAan():
+    os.system('sudo pilight-send -p kaku_switch -i 25544186 -u 0 -t')
+    return
+
+def LampNaastBankUit():
     os.system('sudo pilight-send -p kaku_switch -i 25544186 -u 0 -f')
     return
 
@@ -63,11 +87,16 @@ lighttime = daylightsavingsunrise.strftime('%H:%M')
 
 schedule.every().day.at("08:00").do(PompAan)
 schedule.every().day.at("15:00").do(AqualichtAan)
-schedule.every(10).minutes.do(WaterkokerUit)
-schedule.every(120).minutes.do(GuineaPigVideo)
+schedule.every().day.at(daylightsavingsunset).do(LampNaastBankAan)
+schedule.every().day.at(daylightsavingsunset).do(TvMeubelLampAan)
+
 schedule.every().day.at("22:30").do(AquaLichtUit)
 schedule.every().day.at("23:00").do(PompUit)
+schedule.every().day.at("23:15").do(LampNaastBankUit)
+schedule.every().day.at("23:30").do(TvMeubelLampUit)
 
+schedule.every(10).minutes.do(WaterkokerUit)
+#schedule.every(120).minutes.do(GuineaPigVideo)
 
 while True:
         schedule.run_pending()
