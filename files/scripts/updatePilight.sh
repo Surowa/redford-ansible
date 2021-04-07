90204c034c2e27c0993f897136ae787801b2a2df
Robert Wagensveld <robert.wagensveld@prodrive-technologies.com>
2021-04-07 15:35:28 +0200

    Added easy update scripts

---
+++ files/scripts/updatePilight.sh
@@ -0,0 +1,8 @@
+cd redford-ansible && git pull && cd
+sudo ansible-playbook redford-ansible/playbooks/redfordcam.yml
+sudo ansible-playbook redford-ansible/playbooks/aquamon.yml
+sudo systemctl daemon-reload
+echo 'Restarting Pilight...'
+sudo service pilight restart
+echo 'Restarting redfordschedule...'
+sudo service redfordschedule restart
